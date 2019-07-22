de48fc6d1f2756c24977d70e904a1cfeb3ee4eac
liutao <csuliutao@sina.cn>
2019-07-22 17:20:47 +0800

    视频数据采集时仍有问题，播放可以播放mp4文件

--- app/src/main/java/csu/liutao/ffmpegdemo/medias/ExtractorManager.kt
+++ app/src/main/java/csu/liutao/ffmpegdemo/medias/ExtractorManager.kt
@@ -7,6 +7,7 @@ import java.nio.ByteBuffer
 class ExtractorManager(val path : String, val mineType :String = MediaFormat.MIMETYPE_VIDEO_AVC) {
     private val extractor = MediaExtractor()
     private lateinit var format: MediaFormat
+    private var isStarted = true
 
     init {
         extractor.setDataSource(path)
@@ -26,12 +27,24 @@ class ExtractorManager(val path : String, val mineType :String = MediaFormat.MIM
         return format
     }
 
+    fun stop () {
+        isStarted = false
+    }
+
+    fun restart() {
+        isStarted = true
+    }
+
     fun release() {
         extractor.release()
     }
 
+    /**
+     * 返回-1已经播放完毕， -2仍存在数据，但是当前状态为暂停
+     */
     fun read(buffer: ByteBuffer, offset : Int = 0) : Int{
         val size = extractor.readSampleData(buffer, offset)
+        if (!isStarted) return if (size < 0) -1 else -2
         if (size > 0) extractor.advance()
         return size
     }
