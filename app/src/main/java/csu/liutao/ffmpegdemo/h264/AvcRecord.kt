39117ed24c9147c5a9b478086979a5da8fd8ff5b
liutao <csuliutao@sina.cn>
2019-07-25 09:04:04 +0800

    编码质量提升

--- app/src/main/java/csu/liutao/ffmpegdemo/h264/AvcRecord.kt
+++ app/src/main/java/csu/liutao/ffmpegdemo/h264/AvcRecord.kt
@@ -65,7 +65,7 @@ class AvcRecord(var muxer: MuxerManger, queueSize : Int = 10)  {
             .build()
         cameraMgr.openCamera(context)
         val format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, width, height)
-        format.setInteger(MediaFormat.KEY_BIT_RATE, width * height * 3)
+        format.setInteger(MediaFormat.KEY_BIT_RATE, width * height * 5)
         format.setInteger(MediaFormat.KEY_FRAME_RATE, 25)
         format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible)
         format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1)
