707f17e3c59472b6a9a1ea0e20d8fb1d865a8c9c
liutao <csuliutao@sina.cn>
2019-07-25 14:45:00 +0800

    ...

--- app/src/main/java/csu/liutao/ffmpegdemo/h264/AvcRecord.kt
+++ app/src/main/java/csu/liutao/ffmpegdemo/h264/AvcRecord.kt
@@ -58,7 +58,7 @@ class AvcRecord(var muxer: MuxerManger, queueSize : Int = 10)  {
                 format.setInteger(MediaFormat.KEY_FRAME_RATE, 25)
                 format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible)
                 format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1)
-                format.setInteger(MediaFormat.KEY_ROTATION, 90)
+//                format.setInteger(MediaFormat.KEY_ROTATION, 90) // 只有在输出到surface时才有效
                 codecMgr = CodecManager(format, codecCallback)
                 codecMgr!!.start()
             }
